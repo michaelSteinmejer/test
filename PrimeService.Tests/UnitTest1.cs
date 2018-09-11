2be291b2896ceef229556d71d5b3081bfdb1f15a
Michael Steinmejer <mich86p0@edu.easj.dk>
2018-09-11 11:21:20 +0200

    blah2

--- PrimeService.Tests/UnitTest1.cs
+++ PrimeService.Tests/UnitTest1.cs
@@ -3,11 +3,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PrimeService.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class PrimeService_IsPrimeShould
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
         {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
         }
     }
 }
