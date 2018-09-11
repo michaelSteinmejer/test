f4a10f53e4a13021b66f28807a4d36599d5f4797
Michael Steinmejer <mich86p0@edu.easj.dk>
2018-09-11 11:20:44 +0200

    bjah

--- Primeservice/Primeservice.cs
+++ Primeservice/Primeservice.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 
-namespace Primeservice
+namespace PrimeService
 {
    
         public class PrimeService
         {
             public bool IsPrime(int candidate)
             {
+            if (candidate == 1)
+                {
+                    return false;
+                }
                 throw new NotImplementedException("Please create a test first");
-            }
+        }
         }
     
 
